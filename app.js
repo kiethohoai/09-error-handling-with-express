fe8b80956e656dd2a87540dfc61c4bf7d2b4cf70
kiethohoai <kietho.060193@gmail.com>
2024-09-05 15:04:32 +0700

    022 Data Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const { rateLimit } = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -29,9 +31,18 @@ app.use('/api', limiter);
 // Apply the rate limiting middleware to all requests.
 app.use(limiter);
 
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+
+//  Data Sanitization agaist NoSQL query injection
+app.use(mongoSanitize());
+
+//  Data Sanitization agaist  XSS
+app.use(xss());
+
+// Static File
 app.use(express.static(`${__dirname}/public`));
 
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
