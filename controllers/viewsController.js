9aefd192723711f09a53b04edbcdac85211ff2fd
kiethohoai <kietho.060193@gmail.com>
2024-09-07 18:01:27 +0700

    011 Building the Tour Page - Part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,8 +13,19 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour'
+exports.getTour = catchAsync(async (req, res) => {
+  // 1. Get the data, for the requested tour (includeing reviews & guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
   });
-};
+
+  // 2. Build template
+
+  // 3. Render template using the data from step 1
+
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour',
+    tour: tour
+  });
+});
