102bf43f7d3531a1dee50da6353b00f0c3e32e10
kiethohoai <kietho.060193@gmail.com>
2024-09-07 13:57:31 +0700

    007 Extending Our Base Template with Blocks

--- app.js
+++ app.js
@@ -71,8 +71,19 @@ app.use((req, res, next) => {
 // 3) ROUTES
 app.get('/', (req, res) => {
   res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
+    title: 'Exciting tours for adventurous people'
+  });
+});
+
+app.get('/overview', (req, res) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+app.get('/tour', (req, res) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
   });
 });
 
